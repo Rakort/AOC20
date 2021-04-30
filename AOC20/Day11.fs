6b1acb91c08928e1be2f458aa14b7a07f637cf1d
Александр Мишуров < @DESKTOP-BU00FQG>
2021-04-30 15:21:12 +0300

    Day 11, 13

---
+++ AOC20/Day11.fs
@@ -0,0 +1,96 @@
+﻿module Day11
+open Xunit
+open System
+open Utils
+
+let filePath = "Inputs\Day11.txt"
+
+/// Возвращает элемент
+let getItem (map: string list) x y = 
+    if x < 0 || y < 0 || x >= map.Head.Length || y >= map.Length then '.'
+    else map.[y].[x]
+
+/// Возвращает соседний элемент
+let getNeighbor (map:string list) x y offssetX offsetY = 
+    getItem map (x+offssetX) (y+offsetY)
+
+/// Возвращает соседний элемент в направлении
+let getNeighborDir (map:string list) x y offssetX offsetY = 
+    let rec getItemDir x y =
+        match getItem map x y with
+        | '.' when x >= 0 && y >= 0 && x < map.Head.Length && y < map.Length -> getItemDir (offssetX+x) (offsetY+y)
+        | char -> char
+    getItemDir (offssetX+x) (offsetY+y)
+
+/// Смещения дял поиска соседей
+let neighborOffsets = List.allPairs [0;1;-1] [0;1;-1] |> List.where (fun (x,y) -> not (x=0 && y=0))
+
+/// Возвращает количество занятых соседних мест
+let countNeg (map:string list) funGetNeighbor x y = 
+    let negs = neighborOffsets 
+                |> List.map (fun (ox,oy) -> funGetNeighbor map x y ox oy) 
+                |> List.countBy id |> List.tryFind (fun (ch, i) -> ch = '#')
+    match negs with
+    | Some (_, i) -> i
+    | None -> 0
+
+/// Возвращает состояние места на следующем шаге
+let step (map:string list) maxNeighbor funGetNeighbor y x = 
+    let seat = getItem map x y
+    if seat = '.' then '.'
+    else 
+        let countNeg = countNeg map funGetNeighbor x y 
+        match seat with
+        | 'L' when countNeg = 0 -> '#'
+        | '#' when countNeg >= maxNeighbor -> 'L'
+        | x -> x
+
+/// Возвращает состояние после одного раунда
+let nextStep maxNeighbor funGetNeighbor (map:string list) =
+    [for y = 0 to map.Length-1 do 
+         yield [0..map.Head.Length-1] |> List.map (step map maxNeighbor funGetNeighbor y) |> String.Concat]  
+
+/// Выполняет правила пока люди не перестают двигаться
+let rec occupiedSeats (map: string list) funNextStep =
+    let newMap = funNextStep map
+    if newMap = map then 
+        newMap |> String.Concat |> Seq.countBy id |> Seq.find (fun (ch,_) -> ch = '#') |> snd
+    else 
+        occupiedSeats newMap funNextStep
+
+/// Вычисление первой части задачи
+let calc input =
+    occupiedSeats input (nextStep 4 getNeighbor)
+
+/// Вычисление второй части задачи
+let calc2 input = 
+    occupiedSeats input (nextStep 5 getNeighborDir)
+
+/// Запуск решения задачи
+let run() =
+    let data = readLines filePath |> Seq.toList
+    calc data  |> printfn "%d"
+    calc2 data |> printfn "%d"
+
+[<Fact>]
+let ``Day 11 Test`` () =
+    let data = [
+    "L.LL.LL.LL";
+    "LLLLLLL.LL";
+    "L.L.L..L..";
+    "LLLL.LL.LL";
+    "L.LL.LL.LL";
+    "L.LLLLL.LL";
+    "..L.L.....";
+    "LLLLLLLLLL";
+    "L.LLLLLL.L";
+    "L.LLLLL.LL"]    
+    Assert.Equal(calc data, 37)
+    Assert.Equal(calc2 data, 26) 
+    0
+
+[<Fact>]
+let ``Day 11 Fact`` () =
+    let data = readLines filePath |> Seq.toList
+    Assert.Equal(calc data, 2238)
+    Assert.Equal(calc2 data, 2013)
---
+++ AOC20/Day13.fs
@@ -0,0 +1,59 @@
+﻿module Day13
+open Xunit
+open System
+open Utils
+
+let filePath = "Inputs\Day13.txt"
+
+/// Вычисление первой части задачи
+let calc (input: string list)=
+    let startTime = int input.[0]
+    let buses = input.[1].Split(',') |> Array.where (fun x -> x <> "x") |> Array.map int 
+    let cross = buses |> Array.map (fun x -> x, (startTime/x+1)*x) |> Array.minBy (snd)
+    fst cross * (snd cross - startTime)
+    
+/// Поиск пересечения двух автобусов
+let rec findCrossTwo startValue step bus offset  =
+    let next = startValue + step
+    if (next+offset)%bus=0L then next
+    else findCrossTwo next step bus offset  
+
+/// Возвращает первое пересечение и время до последующих
+let findCrosses (startValue, step) (sndBus, offset)  = 
+    let fstCross = findCrossTwo startValue step sndBus offset
+    let sndCross = findCrossTwo fstCross step sndBus offset
+    fstCross, sndCross-fstCross
+
+/// Поиск пересечения маршрутов автобусов
+let findCross (input: string) = 
+    let buses = input.Split(',') |> Array.mapi (fun i x -> (x, i)) |> Array.where (fun (x,_) -> x <> "x") |> Array.map (fun (f,i) -> (int64 f, int64 i))
+    Array.tail buses |> Array.fold findCrosses (0L, fst buses.[0]) |> fst
+
+/// Вычисление второй части задачи
+let calc2 (input: string list) =  
+    findCross input.[1]
+
+/// Запуск решения задачи
+let run() =
+    let data = readLines filePath |> Seq.toList
+    calc data  |> printfn "%d"
+    calc2 data |> printfn "%d"
+
+[<Fact>]
+let ``Day 13 Test`` () =
+    let data = ["939"; "7,13,x,x,59,x,31,19";]    
+    Assert.Equal(calc data, 295)   
+    Assert.Equal(findCross "13,19", 208L) 
+    Assert.Equal(findCross "17,x,13", 102L) 
+    Assert.Equal(findCross "17,x,x,19", 187L) 
+    Assert.Equal(findCross "17,x,13,19", 3417L) 
+    Assert.Equal(findCross "67,7,59,61", 754018L) 
+    Assert.Equal(findCross "67,x,7,59,61", 779210L) 
+    Assert.Equal(findCross "67,7,x,59,61", 1261476L) 
+    Assert.Equal(findCross "1789,37,47,1889", 1202161486L) 
+
+[<Fact>]
+let ``Day 13 Fact`` () =
+    let data = readLines filePath |> Seq.toList
+    Assert.Equal(calc data, 333)
+    Assert.Equal(calc2 data, 690123192779524L)
